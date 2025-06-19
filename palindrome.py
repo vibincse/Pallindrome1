7514572bb0aedf6767903d03fc5c7f64bca39d64
vibin Rajan <vibin.cse@gmail.com>
2025-06-18 22:05:45 -0400

    deque to validate palindromes like bb
     deque to validate palindromes like bb

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,8 @@
 """
 Validates strings as palindromes.
 """
+from collections import deque
+
 def is_palindrome(value):
     if not isinstance(value, str):
         raise ValueError("Input must be a string")
@@ -8,5 +10,11 @@ def is_palindrome(value):
         return False
     if len(value) == 1:
         return True
+
+    dq = deque(value.lower())  # convert to lowercase
+    while len(dq) > 1:
+        if dq.popleft() != dq.pop():
+            return False
+    return True
 
 
--- test_palindrome.py
+++ test_palindrome.py
@@ -13,3 +13,6 @@ def test_returns_false_on_empty_string():
 
 def test_returns_true_for_single_character():
     assert is_palindrome("a") is True
+
+def test_returns_true_for_two_same_characters():
+    assert is_palindrome("bb") is True
